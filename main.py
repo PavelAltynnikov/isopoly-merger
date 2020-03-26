0f129f457e2fa2858cc44e5822022dc251486a29
Pavel Altynnikov <Robenzon11@gmail.com>
2020-03-27 02:22:00 +0400

    init commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from isopoly_merger.functions import *
+from isopoly_merger.merger import Merger
+
+isopoly_dir = get_pictures_dir()
+RESULT_PATH = os.path.join(isopoly_dir, 'result')
+create_dir(RESULT_PATH)
+
+pictures = get_pictures(isopoly_dir)
+legends = get_legends(isopoly_dir)
+
+if not pictures:
+    print('В данной папке нет картинок для слияния.')
+
+if not legends:
+    print('В данной папке нет файла с легендами в формате "csv"')
+
+if pictures and legends:
+    merge_picture = Merger(create_pictures(isopoly_dir, pictures, legends))
+    merge_picture.save_image(RESULT_PATH)
+
+input('Для завершения нажмите любую клавишу')
---
+++ setup.py
@@ -0,0 +1,28 @@
+from cx_Freeze import setup, Executable
+
+executables = [Executable('main.py',
+                          targetName='Isopoly merger.exe',
+                          icon='resources\Isopoly_merger.ico')]
+
+excludes = ['unittest', 'email', 'html', 'http', 'xml', 'pydoc']
+zip_include_packages = ['collections', 'encodings', 'importlib', 'isopoly_merger', 'logging']
+include_files = [('resources/GOST_Common Italic.ttf', 'resources/GOST_Common Italic.ttf'),
+                 ('resources/GOST_Common.ttf', 'resources/GOST_Common.ttf')]
+
+options = {
+    'build_exe': {
+        'include_msvcr': True,
+        'build_exe': 'build_windows',
+        'excludes': excludes,
+        'zip_include_packages': zip_include_packages,
+        'include_files': include_files,
+    }
+}
+
+setup(
+    name = "Isopoly merger",
+    version = "0.9.0",
+    description = "Слияние результатов расчётов изополей по максимальному значению",
+    executables = executables,
+    options=options
+)
