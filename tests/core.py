ec390644ec7c41ed29cdef315f1d560a1eaef715
PavelAltynnikov <Robenzon11@gmail.com>
2022-02-15 00:05:47 +0400

    add base code for new logick

--- tests/core.py
+++ tests/core.py
@@ -3,41 +3,34 @@ import sys
 
 from PIL import Image
 
-TEST_PICTURES = os.path.join(os.path.dirname(__file__), 'test pictures')
+TEST_FOLDER = os.path.join(os.path.dirname(__file__), 'test pictures')
 SRC = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src')
 sys.path.append(SRC)
+from functions import get_legends
 from isopoly import Isopoly, MergedIsopoly
 
-
 isopolies = [
-    Isopoly(Image.open(os.path.join(TEST_PICTURES, file_name)))
+    Isopoly(Image.open(os.path.join(TEST_FOLDER, file_name)))
     for file_name
-    in os.listdir(TEST_PICTURES)
+    in os.listdir(TEST_FOLDER)
     if file_name.endswith('png')
 ]
 
-legend = {
-    (238, 221, 255): 3.92,
-    (255, 255, 147): 7.85,
-    (0, 206, 0): 9.58,
-    (255, 159, 64): 11.6,
-    (164, 164, 0): 14,
-    (0, 0, 255): 16.6,
-    (255, 0, 255): 19.6,
-}
+legends_data = get_legends(TEST_FOLDER)
 
-[isopoly.set_legend(legend) for isopoly in isopolies]
+for isopoly in isopolies:
+    isopoly.set_data_into_legend(legends_data)
 
-merged_legend = {
-    3.92: (238, 221, 255),
-    7.85: (255, 255, 147),
-    9.58: (0, 206, 0),
-    11.6: (255, 159, 64),
-    14: (164, 164, 0),
-    16.6: (0, 0, 255),
-    19.6: (255, 0, 255),
-    -1: (255, 255, 255)
-}
+for isopoly in isopolies:
+    isopoly.fill_legend()
+
+# for i in isopolies:
+#     print(i._legend._color_number_map)
+
+first_isopoly = isopolies.pop()
+merged_legend = sum([isopoly._legend for isopoly in isopolies], start=first_isopoly._legend)
+
+# print(merged_legend._color_number_map)
 
 merged_isopoly = MergedIsopoly(isopolies[0].size, merged_legend, isopolies)
 merged_isopoly.fill()
--- tests/legend.py
+++ tests/legend.py
@@ -1,3 +1,95 @@
+from PIL.Image import Image
+# from mimetypes import init
+
+
 class Legend:
-    def __init__(self, path):
-        pass
+    line_on_picture = 13
+    uncorrect_colors = [(255, 255, 255), (255, 255, 255, 255), (0, 0, 0), (0, 0, 0, 255)]
+
+    def __init__(self, isopoly_name):
+        self._isopoly_name = isopoly_name
+        self._areas = []
+        self._color_number_map = {}
+        self._number_color_map = {}
+
+    def set_data(self, data):
+        self._areas = data
+
+    def parse_picture(self, picture_line: Image):
+        colors = [
+            same_colors[1]
+            for same_colors
+            in picture_line.getcolors()
+            if same_colors[1] not in Legend.uncorrect_colors
+        ]
+
+        if len(colors) != len(self._areas):
+            raise ColorsDoNotMatchAreasException(self._isopoly_name, colors, self._areas)
+
+        # срез берётся на тот случай если цвет с альфа каналом
+        for color, number in zip(colors, self._areas):
+            self._color_number_map.update({color[:3]: number})  # type: ignore
+            self._number_color_map.update({number: color[:3]})  # type: ignore
+
+    def get_rebar_area(self, color: tuple[int, int, int]) -> float:
+        return self._color_number_map.get(color, -1.0)
+
+    def get_color(self, area: float) -> tuple[int, int, int]:
+        return self._number_color_map.get(area, (255, 255, 255))
+
+    def __add__(self, other: 'Legend'):
+        if isinstance(other, int):
+            return self
+
+        merged_legend = Legend("merged_legend")
+        merged_legend._color_number_map = self._color_number_map.copy()
+
+        for key, value in other._color_number_map.items():
+            if key in merged_legend._color_number_map:
+                value = max(value, merged_legend._color_number_map[key])
+            merged_legend._color_number_map.update({key: value})
+
+        merged_legend._number_color_map = {
+            area: color
+            for color, area
+            in merged_legend._color_number_map.items()
+        }
+
+        return merged_legend
+
+
+class ColorsDoNotMatchAreasException(Exception):
+    def __init__(self, isopoly_name, colors, numbers):
+        super().__init__()
+        self._isopoly_name = isopoly_name
+        self._colors = colors
+        self._numbers = numbers
+
+    def __str__(self):
+        return (
+            f'Для изополя "{self._isopoly_name}" '
+            'не совпадают цвета в легенде и количество чисел в файле csv\n'
+            f'цвета: {self._colors}\n'
+            f'числа: {self._numbers}'
+        )
+
+
+class LegendNotFoundException(Exception):
+    def __init__(self, isopoly_name):
+        super().__init__()
+        self._isopoly_name = isopoly_name
+
+    def __str__(self):
+        return f'Для изополя "{self._isopoly_name}" не найдены данные в файле csv'
+
+
+if __name__ == '__main__':
+    dct1 = {1: 10, 2: 20, 3: 33}
+    dct2 = {4: 40, 2: 22, 3: 30}
+
+    for key, value in dct2.items():
+        if key in dct1:
+            value = max(value, dct1[key])
+        dct1.update({key: value})
+
+    print(dct1)
